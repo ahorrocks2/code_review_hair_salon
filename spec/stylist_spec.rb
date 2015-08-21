0f42db006afc308a8f52293b2644b3e9670086e5
Alyssa Horrocks <am.rocks@outlook.com>
2015-08-21 15:42:49 -0400

    Setup ability to save a stylist to the list of all stylists.

--- spec/stylist_spec.rb
+++ spec/stylist_spec.rb
@@ -8,5 +8,26 @@ describe(Stylist) do
     end
   end
 
+  describe('.all') do
+    it('return an empty array at first') do
+      expect(Stylist.all()).to(eq([]))
+    end
+  end
+
+  describe('#save') do
+    it('add a stylist to the list of stylists') do
+      test_stylist = Stylist.new({:name => 'Sam', :id => nil})
+      test_stylist.save()
+      expect(Stylist.all()).to(eq([test_stylist]))
+    end
+  end
+
+  describe('#==') do
+    it('overwrite the equal comparison') do
+      test_stylist1 = Stylist.new({:name => 'Pippin', :id => nil})
+      test_stylist2 = Stylist.new({:name => 'Pippin', :id => nil})
+      expect(test_stylist1).to(eq(test_stylist2))
+    end
+  end
 
 end
